b4c2e7ae5c189ed3d22972b084bbe3e3c11aa8d3
MariaEduarda15max <eduardadarosa7412@gmail.com>
2026-04-01 05:24:11 -0300

    app terminada

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -1,38 +1,61 @@
 import { useState } from "react"
 
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
 
 
   const handleGoodClick = () => {
     setAll(allClicks.concat('Good'))
-    setGood(good + 1)
+    const updatedGood = good + 1
+    setGood(updatedGood)
+    setTotal(updatedGood + neutral + bad)
   }
 
 
   const handleNeutralClick = () => {
     setAll(allClicks.concat('Neutral'))
-    setNeutral(neutral + 1)
+    const updatedNeutral = neutral + 1
+    setNeutral(updatedNeutral)
+    setTotal(good + updatedNeutral + bad)
   }
   const handleBadClick = () => {
     setAll(allClicks.concat('Bad'))
-    setBad(bad + 1)
+    const updatedBad = bad + 1
+    setBad(updatedBad)
+    setTotal(good + neutral + updatedBad)
   }
 
   return (
     <div>
       <h1>Give Feedback</h1>
-      <button onClick={handleGoodClick}>Good</button>
+      <button onClick={handleGoodClick} text='Good'>Good</button>
       {good}
-      <button onClick={handleNeutralClick}>neutral</button>
+      <button onClick={handleNeutralClick} text='neutral'>Neutral</button>
       {neutral}
-      <button onClick={handleBadClick}>Bad</button>
+      <button onClick={handleBadClick} text='Bad'>Bad</button>
       {bad}
       <p>{allClicks.join(' ')}</p>
+      <History allClicks={allClicks} />
     </div>
     )
 }
