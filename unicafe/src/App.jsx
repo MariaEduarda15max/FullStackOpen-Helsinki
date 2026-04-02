6c1b4fe5e92a228a501254c229fc6c7bb5fc7a57
MariaEduarda15max <eduardadarosa7412@gmail.com>
2026-04-01 21:28:27 -0300

    parte 1.6 unicafe

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -1,3 +1,4 @@
+import { captureOwnerStack } from "react"
 import { useState } from "react"
 
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
@@ -20,74 +21,28 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [allClicks, setAll] = useState([])
-  const [total, setTotal] = useState(0)
 
-
-  const handleGoodClick = () => {
-    setAll(allClicks.concat('Good'))
-    const updatedGood = good + 1
-    setGood(updatedGood)
-    setTotal(updatedGood + neutral + bad)
-  }
-
-
-  const handleNeutralClick = () => {
-    setAll(allClicks.concat('Neutral'))
-    const updatedNeutral = neutral + 1
-    setNeutral(updatedNeutral)
-    setTotal(good + updatedNeutral + bad)
-  }
-  const handleBadClick = () => {
-    setAll(allClicks.concat('Bad'))
-    const updatedBad = bad + 1
-    setBad(updatedBad)
-    setTotal(good + neutral + updatedBad)
-  }
+  // + estadisticas
+  const all = good + neutral + bad 
+  const average = all === 0 ? 0 : (good - bad) / all
+  const positive = all === 0 ? 0 : (good / all) * 100
 
   return (
     <div>
       <h1>Give Feedback</h1>
-      <button onClick={handleGoodClick} text='Good'>Good</button>
-      {good}
-      <button onClick={handleNeutralClick} text='neutral'>Neutral</button>
-      {neutral}
-      <button onClick={handleBadClick} text='Bad'>Bad</button>
-      {bad}
-      <p>{allClicks.join(' ')}</p>
-      <History allClicks={allClicks} />
+      <button onClick = {() => setGood(good + 1)}>Good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
+      <button onClick={() => setBad(bad + 1)}>Bad</button>
+      <h1>Statistics</h1>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>All: {all}</p>
+      <p>Average: {average}</p>
+      <p>Positive: {positive}</p>
     </div>
     )
 }
 
 export default App
 
-
-// const Display = props => <div>{props.value}</div>
-
-// const Button = (props) => (
-//   <button onClick={props.onClick}>
-//     {props.text}
-//   </button>
-// )
-
-// const App = () => {
-//   const [value, setValue] = useState(10)
-  
-// const setToValue = newValue => {
-//   console.log('value now', newValue) //print the new value to console
-//   setValue(newValue)
-// }
-
-
-//   return (
-//     <div>
-//       <Display value={value} />
-//       <Button onClick={() => setToValue(1000)} text="thousand" />
-//       <Button onClick={() => setToValue(0)} text="reset" />
-//       <Button onClick={() => setToValue(value + 1)} text="increment" />
-//     </div>
-//   )
-// }
-
-// export default App
